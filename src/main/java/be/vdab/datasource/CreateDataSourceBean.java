9790219893b7e43ac25343ec5876bf03abf77f8b
kr0ma <karim.romagnani@gmail.com>
2015-09-24 14:13:13 +0200

    updated project for openshift deployment

--- src/main/java/be/vdab/datasource/CreateDataSourceBean.java
+++ src/main/java/be/vdab/datasource/CreateDataSourceBean.java
@@ -2,15 +2,44 @@ package be.vdab.datasource;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@PropertySource("classpath:/dao.properties")
 public class CreateDataSourceBean {
 
-		@Bean
-		DataSource dataSource(){
-			return new JndiDataSourceLookup().getDataSource("jdbc/groenetenen");
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	DataSource dataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+		if (environment.containsProperty("OPENSHIFT_MYSQL_DB_HOST")) {
+			// uitvoering op OpenShift
+			dataSource.setJdbcUrl("jdbc:mysql://" + environment.getProperty("OPENSHIFT_MYSQL_DB_HOST") + ':'
+					+ environment.getProperty("OPENSHIFT_MYSQL_DB_PORT") + '/'
+					+ environment.getProperty("openshift.database"));
+			dataSource.setDriverClassName(environment.getProperty("openshift.driverClassName"));
+			dataSource.setUsername(environment.getProperty("OPENSHIFT_MYSQL_DB_USERNAME"));
+			dataSource.setPassword(environment.getProperty("OPENSHIFT_MYSQL_DB_PASSWORD"));
+		} else {
+			dataSource.setDriverClassName(environment.getProperty("local.driverClassName"));
+			dataSource.setJdbcUrl(environment.getProperty("local.jdbcURL"));
+			dataSource.setUsername(environment.getProperty("local.userName"));
+			dataSource.setPassword(environment.getProperty("local.password"));
 		}
+		return dataSource;
+	}
 }
