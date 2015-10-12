64705b8a30426d7906bd57a72a570cdef0ba7a21
kr0ma <karim.romagnani@gmail.com>
2015-10-12 11:20:45 +0200

    added an example of Spring caching

--- src/main/java/be/vdab/services/CreateServiceBeans.java
+++ src/main/java/be/vdab/services/CreateServiceBeans.java
@@ -1,5 +1,12 @@
 package be.vdab.services;
 
+import java.util.Arrays;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,5 +16,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan
 @EnableTransactionManagement
 @EnableScheduling
+@EnableCaching
 public class CreateServiceBeans {
+	@Bean
+	public SimpleCacheManager simpleCacheManager() {
+		SimpleCacheManager manager = new SimpleCacheManager();
+		Cache cache = new ConcurrentMapCache("filialen");
+		manager.setCaches(Arrays.asList(cache));
+		return manager;
+	}
 }
--- src/main/java/be/vdab/services/FiliaalServiceImpl.java
+++ src/main/java/be/vdab/services/FiliaalServiceImpl.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 
@@ -56,6 +57,7 @@ class FiliaalServiceImpl implements FiliaalService {
 	}
 
 	@Override
+	@Cacheable(cacheNames="filialen")
 	public List<Filiaal> findAll() {
 		return filiaalDAO.findAll(new Sort("naam"));
 	}
